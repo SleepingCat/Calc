4e16e930863d8d163511a76018deab3f4b897106
Mr.Anderson <AlexKhochenkov@gmail.com>
2012-09-20 19:14:42 +0400

    Добавил недостающее
    Signed-off-by: Mr.Anderson <AlexKhochenkov@gmail.com>

---
+++ calcus/Calcus.BusinessLayer/Handler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalcusDTO;
+
+
+namespace Calcus.BusinessLayer
+{
+    public class Handler : IHandler
+    {
+
+
+
+
+        public void Process(operands Op)
+        {
+            switch (Op.Operator)
+            {
+                case "+": this.Add(Op); break;
+                case "-": this.Minus(Op); break;
+                case "*": this.Multiply(Op); break;
+                case "/": this.Division(Op); break;
+
+                default: break;
+            }
+        }
+
+        public void Add(operands Op)
+        {
+            Op.result = Op.firstOperand + Op.secondOperand;
+        }
+
+        public void Minus(operands Op)
+        {
+            Op.result = Op.firstOperand - Op.secondOperand;
+        }
+
+        public void Multiply(operands Op)
+        {
+            Op.result = Op.firstOperand * Op.secondOperand;
+        }
+
+        public void Division(operands Op)
+        {
+            Op.result = Op.firstOperand / Op.secondOperand;
+        }
+    }
+       
+}
---
+++ calcus/Calcus.BusinessLayer/IHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalcusDTO;
+
+namespace Calcus.BusinessLayer
+{
+    public interface IHandler
+    {
+
+        void Process(operands Op);
+                
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Op"></param>
+        /// <returns></returns>
+         void Add(operands Op);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Op"></param>
+        /// <returns></returns>
+        void Minus(operands Op);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Op"></param>
+        /// <returns></returns>
+         void Multiply(operands Op);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Op"></param>
+        /// <returns></returns>
+         void Division(operands Op);
+    }
+}
